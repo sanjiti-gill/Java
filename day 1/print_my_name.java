863e4b2497c63e88672ed7bc6d84dc7532749dba
Sanjiti <sanjitishergill@gmail.com>
2026-01-17 06:40:41 +0530

    Create print_my_name.java

---
+++ day 1/print_my_name.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class InputDemo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        int age = sc.next();
+
+        System.out.println("name is:  " + name);
+    }
+}

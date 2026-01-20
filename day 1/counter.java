473fd036763cbf567eca3f93573297b425600453
Sanjiti <sanjitishergill@gmail.com>
2026-01-20 10:42:49 +0530

    Create counter.java

---
+++ day 1/counter.java
@@ -0,0 +1,16 @@
+class counter{
+    static int count=0;
+
+    counter(){
+        count++;
+        System.out.println("object created, current count:" +count);
+    }
+
+    public static void main(String[] args){
+        counter obj1= new counter();
+        counter obj2= new counter();
+        counter obj3= new counter();
+
+        System.out.println("final count:" + counter.count);
+    }
+}

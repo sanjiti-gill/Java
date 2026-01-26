4dc721b6f91189682fde92a13c5491b5d2a68a07
Sanjiti <sanjitishergill@gmail.com>
2026-01-26 23:36:38 +0530

    Create static_method.java

---
+++ day 2/static_method.java
@@ -0,0 +1,16 @@
+class CounterExample {
+
+    static int count = 0;   // static variable
+
+    // static method
+    static void increment() {
+        count++;
+        System.out.println("Count value: " + count);
+    }
+
+    public static void main(String[] args) {
+        CounterExample.increment();
+        CounterExample.increment();
+        CounterExample.increment();
+    }
+}

7cce10fec2645127f0bb196ba388f3ba12926d1c
Sanjiti <sanjitishergill@gmail.com>
2026-01-27 11:15:54 +0530

    Update wrapper_class.java

--- day 2/wrapper_class.java
+++ day 2/wrapper_class.java
@@ -1,8 +1,19 @@
-class wrapper{
+public class wrapper{
     public static void main(String[] args){
-        int x=5;
-        Integer wrappedX= Integer.valueOf(x);
-        int unwrappedX= wrappedX.intValue();
-        System.out.println("wrapped: " + wrappedX + ", unwrapped: " + unwrappedX);
+        int i=3;
+        double d=2.4;
+        System.out.println("Integer i: " +i);
+        System.out.println("Double d: " +d);
+        Integer i1=Integer.valueOf(i);
+        Double d1= Double.valueOf(d);
+        System.out.println("Integer i: " +i1);
+        System.out.println("Double d: " +d1);
+        String str1="123";
+        String str2="345";
+        System.out.println("Answer is: "+(str1+str2));
+        int s1=Integer.parseInt(str1);
+        int s2=Integer.parseInt(str2);
+        System.out.println("Answer is: " +(s1+s2));
+
     }
 }

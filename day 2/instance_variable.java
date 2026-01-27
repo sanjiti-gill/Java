61e9463e40b8c6b41a39f245a63d4cb22781fae8
Sanjiti <sanjitishergill@gmail.com>
2026-01-27 22:26:22 +0530

    Create instance_variable.java

---
+++ day 2/instance_variable.java
@@ -0,0 +1,30 @@
+class bank{
+
+    String acc_holder;
+    int acc_no;
+    int balance;
+
+    bank(String name, int num, int bal){
+        acc_holder=name;
+        acc_no=num;
+        balance=bal;
+    }
+
+    void display(){
+        System.out.println("name is: "+ acc_holder);
+        System.out.println("account number is: " + acc_no);
+        System.out.println("balance is: " + balance);
+    }
+
+    public static void main(String[] args){
+
+        bank b1= new bank("sanjiti", 101,1000);
+        bank b2= new bank("riya", 102, 2000);
+        bank b3= new bank("ram", 103, 3000);
+
+        b1.display();
+        b2.display();
+        b3.display();
+    }
+
+}

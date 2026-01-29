65032ac2cd6cf2c656634f72543b9652e85b5322
Sanjiti <sanjitishergill@gmail.com>
2026-01-29 20:13:24 +0530

    Update parameter_passing_in_java.java

--- day 2/parameter_passing_in_java.java
+++ day 2/parameter_passing_in_java.java
@@ -1,12 +1,17 @@
-class test{
-    void modifyvalue(int x){
-        x=x+10;
+//pass by value
+class Test {
+
+    void change(int x) {
+        x = x + 10;
+        System.out.println("Inside method x = " + x); //output=15
     }
 
-    public static void main(String[] args){
-        int a=5;
-        test obj= new test();
-        obj.modifyvalue(a);
-        System.out.println(a);
+    public static void main(String[] args) {
+        int a = 5;
+
+        Test obj = new Test();
+        obj.change(a);
+
+        System.out.println("Outside method a = " + a);    //output=5
     }
 }

eb4a746bd6661ae93f896ef67394af42806b3295
Sanjiti <sanjitishergill@gmail.com>
2026-02-17 10:26:07 +0530

    Create exception_handling.java

---
+++ basics/exception_handling.java
@@ -0,0 +1,20 @@
+class exception {
+    static void fun(){
+        try{
+            throw new NullPointerException("demo");
+        }
+        catch(NullPointerException e){
+            System.out.println("Caught inside fun().");
+            throw e;
+        }
+    }
+
+    public static void main(String args[]){
+        try{
+            fun();
+        }
+        catch(NullPointerException e){
+            System.out.println("caught in main");
+        }
+    }
+}

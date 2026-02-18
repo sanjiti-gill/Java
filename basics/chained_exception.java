f45eb1219e53bb354b5db142bd34fc509f6007f8
Sanjiti <sanjitishergill@gmail.com>
2026-02-18 15:04:52 +0530

    Create chained_exception.java

---
+++ basics/chained_exception.java
@@ -0,0 +1,16 @@
+public class EXCP{
+    public static void main(String[] args){
+        try{
+            int[] n=new int[5];
+            int divisor=0;
+
+            for(int i=0;i<n.length;i++){
+                int res=n[i]/divisor;
+                System.out.println(res);
+            }
+        }
+        catch (ArithmeticException e){
+            throw new RuntimeException("error: divison by zero occured", e);
+        }
+    }
+}
